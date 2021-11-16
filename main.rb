0c14cd1407de0385c46a04dc2a010dfe4c158687
valentin_zet <xerrox@yandex.ru>
2021-11-16 11:44:53 +0300

    notepad updated to use with SQL

--- main.rb
+++ main.rb
@@ -1,4 +1,13 @@
-if (Gem.win_platform?)
+# encoding: utf-8
+#
+# Программа «Блокнот», демонстрирующая наследование классов в ruby.
+# Версия 2.0, хранящая свои данные в базе данных SQLite
+# Этот скрипт создает новые записи, чтением занимается другой скрипт
+#
+# (с) goodprogrammer.ru
+#
+# Этот код необходим только при использовании русских букв на Windows
+if Gem.win_platform?
   Encoding.default_external = Encoding.find(Encoding.locale_charmap)
   Encoding.default_internal = __ENCODING__
 
@@ -7,31 +16,39 @@ if (Gem.win_platform?)
   end
 end
 
-require_relative "lib/post"
-require_relative "lib/link"
-require_relative "lib/memo"
-require_relative "lib/task"
+# Подключаем класс Post и его детей: Memo, Link, Task
+require_relative 'lib/post'
+require_relative 'lib/memo'
+require_relative 'lib/link'
+require_relative 'lib/task'
 
+# Здороваемся с пользователем и спрашиваем у него, какую запись он хочет создать
 puts 'Привет, я твой блокнот!'
+puts 'Версия 2, записываю новые записи в базу SQLite'
 puts
 puts 'Что хотите записать в блокнот?'
 
-choices = Post.post_types
+# Выводим массив возможных типов Записи (поста) с помощью метода post_types
+# класса Post, который теперь возвращает не массив классов, а хэш.
+choices = Post.post_types.keys
 
 choice = -1
-
-until choice.between?(0, choices.size)
+until choice >= 0 && choice < choices.size
   choices.each_with_index do |type, index|
     puts "\t#{index}. #{type}"
   end
-
-  choice = STDIN.gets.chomp.to_i
+  choice = gets.to_i
 end
 
-entry = Post.create(choice)
+# Как только выбор сделан, мы можем создать запись нужного типа, передав выбор
+# строку с название класса в статический метод create класса Post.
+entry = Post.create(choices[choice])
 
-entry.read_from_colsole
+entry.read_from_console
 
-entry.save
+# Сохраняем пост в базу данных
+rowid = entry.save_to_db
 
-puts "Ура, ваша запись сохранена!"
+puts "Запись сохранена в базе, id = #{rowid}"
+#
+# Как обычно, при использовании классов программа выглядит очень лаконично!
